8934adfed9ca6bb74be777ae797ddad00b6c30dc
ericmlantz <ericmlantz@gmail.com>
2022-06-25 11:54:41 -0400

    heroku test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 const express = require("express");
 const cors = require('cors')
 const app = express();
@@ -11,7 +11,11 @@ const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
 const uri = process.env.MONGODB_URI
 
 app.use(cors())
+
 app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 // const express = require('express')
